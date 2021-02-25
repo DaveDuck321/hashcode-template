4bee433cc00cbad62235d1cdcf5412a27ae14465
DaveDuck321 <tg443@cam.ac.uk>
2021-02-25 12:25:55 -0600

    light.set_open draft

--- Structure.py
+++ Structure.py
@@ -3,14 +3,17 @@
     all_light = []
 
 class light:
-    def __init__(self, street, junction):
-        street = 
-        junction = 
-        is_open = [None]*len(simulation)
-        open_used = [False] * len(simulation)
+    def __init__(self, simulation_length, street, junction):
+        self.street = street
+        self.junction = junction
+        self.is_open = [None] * simulation_length
+        self.open_used = [False] * simulation_length
 
-    def set_open(self):
+    def set_open(self, time):
+        assert self.is_open[time] is None
+
+        self.is_open[time] = True
         for light in self.junction:
             if light.street == self.street:
                 continue
-
+            light.self.is_open[time] = False
