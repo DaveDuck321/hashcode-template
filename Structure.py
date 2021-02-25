ab12520f0678935e48b61f92f7ad957f89fff270
DaveDuck321 <tg443@cam.ac.uk>
2021-02-25 12:16:15 -0600

    LETS GOOO

---
+++ Structure.py
@@ -0,0 +1,16 @@
+ class junction:
+    junction_id = 
+    all_light = []
+
+class light:
+    def __init__(self, street, junction):
+        street = 
+        junction = 
+        is_open = [None]*len(simulation)
+        open_used = [False] * len(simulation)
+
+    def set_open(self):
+        for light in self.junction:
+            if light.street == self.street:
+                continue
+
